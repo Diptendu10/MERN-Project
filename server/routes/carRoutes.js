bb904ac8827adef40c3e8feebee40bb9490a1758
Diptendu Mondal <diptendum10@gmail.com>
2026-06-02 23:54:55 +0530

    Fix navbar routing

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -1,27 +1,3 @@
-// const express = require("express");
-// const upload =
-//   require("../middleware/upload");
-// const router = express.Router();
-
-// const {
-//   addCar,
-//   getCars,
-//   deleteCar,
-//   updatePrice,
-// } = require("../controllers/carController");
-
-// router.post("/add", upload.single("image"),addCar);
-
-// router.delete("/:id", deleteCar);
-
-// router.put("/:id", updatePrice);
-
-// router.get("/", getCars);
-
-// module.exports = router;
-
-
-
 const express = require("express");
 const router = express.Router();
 
